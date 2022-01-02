cc561ab9f7c2c1f84f3be638a1ccd7dd8d32031f
maikhanh <maikhanh123@gmail.com>
2022-01-02 11:25:43 +0700

    bootstrap and style

---
+++ demo/pages/index.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function IndexPage() {
+  // if (1 === 0) {
+  //   const [x, setX] = useState();
+  // }
+  return <div>Hello world</div>;
+}
+
+export default IndexPage;
