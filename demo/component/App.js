dd4b0a1892a7a4341e3f8abc34a8f545461ee676
maikhanh <maikhanh123@gmail.com>
2022-01-02 17:35:04 +0700

    create note in useNotes

--- demo/component/App.js
+++ demo/component/App.js
@@ -1,8 +1,9 @@
 import NoteList from "./NoteList.js";
 import useNotes from "../hooks/useNotes";
+import Menu from "./menu.js";
 
 function App() {
-  const { notesData, notesDataError } = useNotes();
+  const { notesData, notesDataError, createNote } = useNotes();
 
   if (notesDataError) {
     return <div>error: {notesDataError}</div>;
@@ -10,8 +11,22 @@ function App() {
   if (!notesData) {
     return <div>...loading</div>;
   }
+
+  function createNoteFn() {
+    const timeOfDay = new Date().toLocaleDateString("en", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+    createNote(
+      `Note at ${timeOfDay}`,
+      `This sample note create at ${timeOfDay}`
+    );
+  }
+
   return (
     <div className="container">
+      <Menu createNoteFn={createNoteFn} />
       <NoteList notesData={notesData} />
     </div>
   );
